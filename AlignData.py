543144187a52d0167821a6bfecdea6227d456517
Adam Washington <adam.washington@stfc.ac.uk>
2017-08-23 17:12:19 +0100

    Document AlignData

--- AlignData.py
+++ AlignData.py
@@ -1,3 +1,4 @@
+""" This module calculates the alignment of the sample images"""
 import json
 import numpy as np
 from PyQt5.QtCore import pyqtProperty, pyqtSignal, pyqtSlot, QPointF
@@ -7,8 +8,9 @@ from PyQt5.QtQuick import QQuickItem
 
 
 class AlignData(QQuickItem):
+    """This class models the alignment of an image"""
     def __init__(self, parent=None):
-        super().__init__(parent)
+        super(AlignData, self).__init__(parent)
         self.setFlag(QQuickItem.ItemHasContents, True)
         self._p1 = QPointF(0.1, 0.1)
         self._p2 = QPointF(100, 0.1)
@@ -24,17 +26,21 @@ class AlignData(QQuickItem):
 
     @pyqtSlot(float, float)
     def select(self, x, y):
-        d1 = (self._p1.x()-x)**2+(self._p1.y()-y)**2
-        d2 = (self._p2.x()-x)**2+(self._p2.y()-y)**2
-        d3 = (self._p3.x()-x)**2+(self._p3.y()-y)**2
-        d4 = (self._p4.x()-x)**2+(self._p4.y()-y)**2
-        if d1 < d2 and d1 < d3 and d1 < d4:
+        """Find the point closest to a coordinate and move that point to the
+        new coordinate"""
+        distance1 = (self._p1.x()-x)**2+(self._p1.y()-y)**2
+        distance2 = (self._p2.x()-x)**2+(self._p2.y()-y)**2
+        distance3 = (self._p3.x()-x)**2+(self._p3.y()-y)**2
+        distance4 = (self._p4.x()-x)**2+(self._p4.y()-y)**2
+        if distance1 < distance2 and \
+           distance1 < distance3 and \
+           distance1 < distance4:
             self._p1.setX(x)
             self._p1.setY(y)
-        elif d2 < d3 and d2 < d4:
+        elif distance2 < distance3 and distance2 < distance4:
             self._p2.setX(x)
             self._p2.setY(y)
-        elif d3 < d4:
+        elif distance3 < distance4:
             self._p3.setX(x)
             self._p3.setY(y)
         else:
@@ -45,6 +51,8 @@ class AlignData(QQuickItem):
     realigned = pyqtSignal()
 
     def linreg(self):
+        """Use a linear regression to find the desired transformation between
+        the control points and the corners of the reference image"""
         ys = [0, self._newsize, self._newsize+self._newsize*1.0j,
               self._newsize*1.0j]
         xs = [self.toComplex(p) for p in
@@ -54,17 +62,17 @@ class AlignData(QQuickItem):
         self._slope = np.sum((xs-xmean)*np.conj(ys-ymean)) / \
             np.sum(np.abs(xs-xmean)**2)
         self._intercept = ymean - self._slope*xmean
-        print(np.abs(self._slope))
-        print(np.angle(self._slope))
-        print(self._intercept)
         self.realigned.emit()
 
     @staticmethod
     def toComplex(v):
+        """Turn a Qt QPoint into a complex number"""
         return v.x()+1j*v.y()
 
     @pyqtProperty('qreal', notify=realigned)
-    def width(self): return self._width
+    def width(self):
+        """The width of the photorgaph"""
+        return self._width
 
     @width.setter
     def width(self, p):
@@ -72,7 +80,9 @@ class AlignData(QQuickItem):
         self.linreg()
 
     @pyqtProperty('qreal', notify=realigned)
-    def height(self): return self._height
+    def height(self):
+        """The height of the photograph"""
+        return self._height
 
     @height.setter
     def height(self, p):
@@ -80,7 +90,9 @@ class AlignData(QQuickItem):
         self.linreg()
 
     @pyqtProperty('qreal', notify=realigned)
-    def newsize(self): return self._newsize
+    def newsize(self):
+        """The width and height of the cropped reference image"""
+        return self._newsize
 
     @newsize.setter
     def newsize(self, p):
@@ -88,7 +100,9 @@ class AlignData(QQuickItem):
         self.linreg()
 
     @pyqtProperty('QPointF', notify=realigned)
-    def p1(self): return self._p1
+    def p1(self):
+        """The control point for the top left corner"""
+        return self._p1
 
     @p1.setter
     def p1(self, p):
@@ -96,7 +110,9 @@ class AlignData(QQuickItem):
         self.linreg()
 
     @pyqtProperty('QPointF')
-    def p2(self): return self._p2
+    def p2(self):
+        """The control point for the top right corner"""
+        return self._p2
 
     @p2.setter
     def p2(self, p):
@@ -104,7 +120,9 @@ class AlignData(QQuickItem):
         self.linreg()
 
     @pyqtProperty('QPointF')
-    def p3(self): return self._p3
+    def p3(self):
+        """The control point for the bottom right corner"""
+        return self._p3
 
     @p3.setter
     def p3(self, p):
@@ -112,7 +130,9 @@ class AlignData(QQuickItem):
         self.linreg()
 
     @pyqtProperty('QPointF')
-    def p4(self): return self._p4
+    def p4(self):
+        """The control point for the bottom left corner"""
+        return self._p4
 
     @p4.setter
     def p4(self, p):
@@ -121,38 +141,35 @@ class AlignData(QQuickItem):
 
     @pyqtProperty(str)
     def jsonString(self):
-        d = {"p1": {"x": self._p1.x(), "y": self._p1.y()},
-             "p2": {"x": self._p2.x(), "y": self._p2.y()},
-             "p3": {"x": self._p3.x(), "y": self._p3.y()},
-             "p4": {"x": self._p4.x(), "y": self._p4.y()}}
-        return json.dumps(d)
+        """A JSON serialisation of the object's current state"""
+        value = {"p1": {"x": self._p1.x(), "y": self._p1.y()},
+                 "p2": {"x": self._p2.x(), "y": self._p2.y()},
+                 "p3": {"x": self._p3.x(), "y": self._p3.y()},
+                 "p4": {"x": self._p4.x(), "y": self._p4.y()}}
+        return json.dumps(value)
 
     @jsonString.setter
-    def jsonString(self, v):
-        d = json.loads(v)
-        self._p1 = QPointF(d["p1"]["x"], d["p1"]["y"])
-        self._p2 = QPointF(d["p2"]["x"], d["p2"]["y"])
-        self._p3 = QPointF(d["p3"]["x"], d["p3"]["y"])
-        self._p4 = QPointF(d["p4"]["x"], d["p4"]["y"])
+    def jsonString(self, value):
+        value = json.loads(value)
+        self._p1 = QPointF(value["p1"]["x"], value["p1"]["y"])
+        self._p2 = QPointF(value["p2"]["x"], value["p2"]["y"])
+        self._p3 = QPointF(value["p3"]["x"], value["p3"]["y"])
+        self._p4 = QPointF(value["p4"]["x"], value["p4"]["y"])
         self.linreg()
-
-    def _hangle(self, b, a):
-        dx = b.x() - a.x()
-        dy = b.y() - a.y()
-        return 180*np.arctan2(dy, dx)/np.pi
 
     @pyqtProperty('qreal', notify=realigned)
     def angle(self):
-        print(np.angle(self._slope)*180/np.pi)
+        """The rotation correction angle for the reference image"""
         return np.angle(self._slope)*180/np.pi
 
     @pyqtProperty('qreal', notify=realigned)
     def scale(self):
-        print("Scale:", np.abs(self._slope))
+        """The appropriate magnification for the reference image"""
         return np.abs(self._slope)
 
     @pyqtProperty('QPointF', notify=realigned)
     def translate(self):
+        """The position offset for the reference image"""
         old_center = self._newsize * (0.5 + 0.5j)
         center = (old_center-self._intercept)/self._slope
         offset = (self._width/2 + self._height/2*1.0j) - center
