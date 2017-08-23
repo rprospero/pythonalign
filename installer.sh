bd590543ef5b2b8806b99708dde553c52b562f6e
Adam Washington <adam.washington@stfc.ac.uk>
2017-08-23 17:34:27 +0100

    Create run script

--- installer.sh
+++ installer.sh
@@ -3,6 +3,7 @@
 curl https://codeload.github.com/rprospero/pythonalign/zip/master > master.zip
 unzip -j master.zip
 rm master.zip
+chmod +x run.sh
 
 pip3 install --user virtualenv
 python3 -m virtualenv ./
